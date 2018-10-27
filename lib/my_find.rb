681883655c0c2499c24c729d00cd4adbe7acc7f6
Roberto Samia <bojosteph@gmail.com>
2018-10-27 14:15:17 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,9 +4,9 @@ def my_find(collection)
   i = 0
   while i < collection.length
     return collection[i] if yield(collection[i])
-    i = i+ 1
+    i = i + 1
   end
 end
 
-collection = (1..100).to_a
+#collection = (1..100).to_a
 my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }learn
